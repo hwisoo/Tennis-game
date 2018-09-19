c5c0a6e2402753880393acb65db42a6be5e2ebac
James Cho <hwisoocho@gmail.com>
2018-09-18 19:01:27 -0700

    movement

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
+var ballSpeedX = 5;
 
 window.onload = function() {
-    console.log("Hello World!");
-    canvas = this.document.getElementById('gameCanvas');
+    canvas = document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
     
-    setInterval(drawEverything, 50);
-    drawEverything();
+    var framesPerSecond = 30;
+    setInterval(function(){
+        moveEverything();
+        drawEverything();
+    }, 1000/framesPerSecond);
+}
+
+
+function moveEverything() {
+    ballX = ballX + ballSpeedX;
+    if (ballX > canvas.width) {
+        ballSpeedX = - ballSpeedX
+    }
+    if (ballX < 0) {
+        ballSpeedX = -ballSpeedX
+    }
 }
 
 function drawEverything() {
-    ballX = ballX + 5;
-
-    console.log(ballX)
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0,0, canvas.width, canvas.height);
     canvasContext.fillStyle = 'white';
-    canvasContext.fillRect(225, 210, 200, 200);
+    canvasContext.fillRect(0, 210, 10, 100);
     canvasContext.fillStyle = 'red';
     canvasContext.fillRect(ballX, 100, 10, 10);
 }
