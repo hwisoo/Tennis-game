640d50bd5277cf6a62d127f576bf61ffa95dd6fa
James Cho <hwisoocho@gmail.com>
2018-09-19 11:34:59 -0700

    Paddle AI

--- index.js
+++ index.js
@@ -44,10 +44,20 @@ function ballReset() {
     ballY = canvas.height/2;
 }
 
+function computerMovement(){
+    var paddle2YCenter = paddle2Y + (PADDLE_HEIGHT/2);
+    if(paddle2Y < ballY) {
+        paddle2Y += 6;
+    } else {
+        paddle2Y -= 6;
+    }
+}
 
 function moveEverything() {
-    ballX = ballX + ballSpeedX;
-    ballY = ballY + ballSpeedY;
+    computerMovement();
+
+    ballX += ballSpeedX;
+    ballY += ballSpeedY;
   
     if (ballX < 0) {
         if(ballY > paddle1Y && 
