37b14c41561434b67437ba043f9542f9c2ac8895
James Cho <hwisoocho@gmail.com>
2018-09-18 18:43:31 -0700

    draw functions

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var canvas;
 var canvasContext;
+var ballX = 50;
 
 window.onload = function() {
     console.log("Hello World!");
     canvas = this.document.getElementById('gameCanvas');
     canvasContext = canvas.getContext('2d');
+    
+    setInterval(drawEverything, 50);
+    drawEverything();
+}
+
+function drawEverything() {
+    ballX = ballX + 5;
+
+    console.log(ballX)
     canvasContext.fillStyle = 'black';
     canvasContext.fillRect(0,0, canvas.width, canvas.height);
-    canvasContext.fillStyle = 'red';
-    canvasContext.fillRect(200, 200, 50, 25);
     canvasContext.fillStyle = 'white';
-    canvasContext.fillRect(230, 230, 10, 10);
+    canvasContext.fillRect(225, 210, 200, 200);
+    canvasContext.fillStyle = 'red';
+    canvasContext.fillRect(ballX, 100, 10, 10);
 }
