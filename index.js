32620e1e4c3f07a872d72dfb0213e83ef90490aa
James Cho <hwisoocho@gmail.com>
2018-09-18 18:25:10 -0700

    game canvas

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert("hello");
+var canvas;
+var canvasContext;
+
+window.onload = function() {
+    console.log("Hello World!");
+    canvas = this.document.getElementById('gameCanvas');
+    canvasContext = canvas.getContext('2d');
+    canvasContext.fillStyle = 'black';
+    canvasContext.fillRect(0,0, canvas.width, canvas.height);
+    canvasContext.fillStyle = 'red';
+    canvasContext.fillRect(200, 200, 50, 25);
+    canvasContext.fillStyle = 'white';
+    canvasContext.fillRect(230, 230, 10, 10);
+}
